0c5b65d500ec9a59297854236309166a4a493bd3
Ankit Yadav <ankitwrk@gmail.com>
2016-06-09 23:10:00 +0530

    Adding croos origin support in api

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ var express = require('express');
 // Express app instance
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+    app.use(allowCrossDomain);
+
+
 // Morgan instance to log each request on terminal
 var morgan = require('morgan');
 
